3301814fa109f0055e1177a32b40cb51a42962f2
Carlos Guillermo Mejia <Rafikyop@gmail.com>
2022-12-04 12:12:07 -0500

    Organizacion de codigo del home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,62 +1,67 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link, useNavigate } from 'react-router-dom'
-import Swal from 'sweetalert2'
-import { actionGetrestaurantesAsync } from '../../redux/actions/restaurantsActions'
-import { actionLogout, actionUserLogOutAsync } from '../../redux/actions/userActions'
-import Navbar from '../restaurants/Navbar'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { actionGetrestaurantesAsync } from "../../redux/actions/restaurantsActions";
+import {
+  actionLogout,
+  actionUserLogOutAsync,
+} from "../../redux/actions/userActions";
+import Navbar from "../restaurants/Navbar";
 import "./home.scss";
 
 const Home = ({ isAuthentication }) => {
-const lista=["pizzas juan","pizza picolo","pizza carlota","carlos pizza","pizza picolo",
-"pizza dominos","pizza daikmaku"," pizza lauras", " kakos pizza"]
-const[dado, setDado]=useState(false)  
-const [userFunctions,setUserFunctions]=useState(false)
-const disptach=useDispatch()
-const userStore = useSelector((store) => store.userStore);
-const {restaurantes}=useSelector((store)=>store.restaurantStore)
-const navigate=useNavigate()
-const logOut=()=>{
-  disptach(actionUserLogOutAsync())
-}
+  const lista = [
+    "pizzas juan",
+    "pizza picolo",
+    "pizza carlota",
+    "carlos pizza",
+    "pizza picolo",
+    "pizza dominos",
+    "pizza daikmaku",
+    " pizza lauras",
+    " kakos pizza",
+  ];
+  const [dado, setDado] = useState(false);
+  const [userFunctions, setUserFunctions] = useState(false);
+  const disptach = useDispatch();
+  const userStore = useSelector((store) => store.userStore);
+  const { restaurantes } = useSelector((store) => store.restaurantStore);
+  const navigate = useNavigate();
+  const logOut = () => {
+    disptach(actionUserLogOutAsync());
+  };
 
-useEffect(() => {
-  
-  if (isAuthentication) {
-    setUserFunctions(true)
-    
-  }else{
-    setUserFunctions(false)
-  }
-  
-}, [isAuthentication,userFunctions])
+  useEffect(() => {
+    if (isAuthentication) {
+      setUserFunctions(true);
+    } else {
+      setUserFunctions(false);
+    }
+  }, [isAuthentication, userFunctions]);
 
   // useEffect(() => {
   // console.log(userStore);
   // if (!userStore.name) {
-  
+
   //   navigate('/login')
   //   console.log(userStore.name);
   // }
   // }, [userStore])
- 
+
   useEffect(() => {
     if (!restaurantes.length) {
-      disptach(actionGetrestaurantesAsync())
-   console.log(restaurantes);
+      disptach(actionGetrestaurantesAsync());
+      console.log(restaurantes);
     }
-   
-   
-  }, [restaurantes])
- 
-console.log(restaurantes);
+  }, [restaurantes]);
+
+  console.log(restaurantes);
   useEffect(() => {
     console.log(userStore);
-   
-  }, [userStore])
-  
-  
-  const changeDado=()=>{
+  }, [userStore]);
+
+  const changeDado = () => {
     setDado(true);
     setTimeout(() => {
       const x = Math.floor(Math.random() * lista.length);
@@ -69,62 +74,58 @@ console.log(restaurantes);
     setTimeout(() => {
       setDado(false);
     }, 2000);
-  
-  }
+  };
   const handleDicePage = () => {
     navigate("/dice");
   };
-  return ( 
-    <> 
-    <Navbar />
-    <section className="home">
-          <div className="home__section1">
-            <h1 className="home__title">¿Qué comeremos hoy?</h1>
-            <p className="home__text">
-              Hey! Los invitamos a que tiren el dado y encuentren un lugar
-              diferente en donde puedan comer el dia de hoy. Que esperas?!
-            </p>
-          </div>
-          <div className="home__section2">
-            <div className="contenedor2">
-              <div className={"dado2"}>
-                <div className="lado uno"></div>
-                <div className="lado dos"></div>
-                <div className="lado tres"></div>
-                <div className="lado cuatro"></div>
-                <div className="lado cinco"></div>
-                <div className="lado seis"></div>
-              </div>
+  return (
+    <>
+      <Navbar />
+      <section className="home">
+        <div className="home__section1">
+          <h1 className="home__title">¿Qué comeremos hoy?</h1>
+          <p className="home__text">
+            Hey! Los invitamos a que tiren el dado y encuentren un lugar
+            diferente en donde puedan comer el dia de hoy. Que esperas?!
+          </p>
+        </div>
+        <div className="home__section2">
+          <div className="contenedor2">
+            <div className={"dado2"}>
+              <div className="lado uno"></div>
+              <div className="lado dos"></div>
+              <div className="lado tres"></div>
+              <div className="lado cuatro"></div>
+              <div className="lado cinco"></div>
+              <div className="lado seis"></div>
             </div>
-            <button onClick={handleDicePage} className="home__ThrowDice">
-              Tira el dado!
-            </button>
           </div>
-        </section>
-    
-   
- 
+          <button onClick={handleDicePage} className="home__ThrowDice">
+            Tira el dado!
+          </button>
+        </div>
+      </section>
 
-
-
-
-
-
-
-
-
-   { isAuthentication?<button onClick={logOut}> salir</button>:""}
-   {isAuthentication && userStore.admin ?<div>
-   <button > Add restaurant</button>
-   <button > Edit restaurant</button> </div>:"" }
-   {isAuthentication && !userStore.admin ?<div>
-   <button > Favoritos</button>
-    </div>:"" }
-{isAuthentication?userStore.displayName:""}
-{isAuthentication?"":<Link to={'/login'} >Ingresar </Link>}
-    
+      {isAuthentication ? <button onClick={logOut}> salir</button> : ""}
+      {isAuthentication && userStore.admin ? (
+        <div>
+          <button> Add restaurant</button>
+          <button> Edit restaurant</button>{" "}
+        </div>
+      ) : (
+        ""
+      )}
+      {isAuthentication && !userStore.admin ? (
+        <div>
+          <button> Favoritos</button>
+        </div>
+      ) : (
+        ""
+      )}
+      {isAuthentication ? userStore.displayName : ""}
+      {isAuthentication ? "" : <Link to={"/login"}>Ingresar </Link>}
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
