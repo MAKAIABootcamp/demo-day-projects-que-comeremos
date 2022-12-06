9c2dcf4103236ab091a7107bf658d83d8e4173ad
unknown <lauraqp.00918@hotmail.com>
2022-12-05 21:50:02 -0500

    cambios en componente restaurant

--- src/components/restaurant/Restaurant.jsx
+++ src/components/restaurant/Restaurant.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { actionGetrestaurantesAsync } from '../../redux/actions/restaurantsActions'
 import NavbarDice from '../dice/NavbarDice'
-import Rating from './Rating'
+import RatingComponent from './Rating'
 import './restaurant.scss'
 
 const Restaurant = () => {
@@ -36,62 +36,22 @@ const Restaurant = () => {
   
   
   return (
+    <>
+    <NavbarDice/>
     <div className="restaurant">
-    
       <section className="restaurant__section">
-        <h1>{restaurant.name?restaurant.name:""} </h1>
+        <h1 className='restaurant__h1'>{restaurant.name?restaurant.name:""} </h1>
+        <RatingComponent/>
         <section className="restaurant__links">
           <p>¡Me gusta!</p>
           <p>Compartir</p>
         </section>
-        <p><img src="https://res.cloudinary.com/dpssc03mq/image/upload/v1670254421/clavo_xmogpe.png" alt="" className="restaurant__icono"/> Dirección:</p>
-        <p><img src="https://res.cloudinary.com/dpssc03mq/image/upload/v1670254421/reloj-con-sentido-horario_vnhs4y.png" alt="" className="restaurant__icono"/> Horario de atención:</p>
+        <p><img src="https://res.cloudinary.com/dpssc03mq/image/upload/v1670254421/clavo_xmogpe.png" alt="" className="restaurant__icono"/> Sedes:{restaurant.sedes?restaurant.sedes:""}</p>
+        <p><img src="https://res.cloudinary.com/dpssc03mq/image/upload/v1670254421/reloj-con-sentido-horario_vnhs4y.png" alt="" className="restaurant__icono"/> Horario de atención:{restaurant.horarios?restaurant.horarios:""}</p>
         <p><img src="https://res.cloudinary.com/dpssc03mq/image/upload/v1670254421/contacto-telefonico_mrd0gb.png" alt="" className="restaurant__icono"/> Contacto:</p>
       </section>
 
 
-      {/* <article className='restaurant__article'>
-      
-      <Carousel slide={false} className="restaurant__carousel">
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Second slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-      </article> */}
       <article className='restaurant__article'>
       
 
@@ -119,6 +79,7 @@ const Restaurant = () => {
       
       </article>
     </div>
+    </>
   );
 };
 
